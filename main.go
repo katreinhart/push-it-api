c4afa7a80054b710ab494cbd4a643eac9cd341ad
Kat Reinhart <kat.reinhart@gmail.com>
2018-01-18 15:47:19 -0800

    Start building router

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	fmt.Println("Hello world!")
+	var port string
+	if os.Getenv("PORT") != "" {
+		port = os.Getenv("PORT")
+	} else {
+		port = "8080"
+	}
+
+	r := mux.NewRouter().StrictSlash(true)
+
+	r.HandleFunc("/", homeHandler)
+
+	http.ListenAndServe(":"+port, r)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("{\"message\": \"Hello world\"}"))
 }
--- model/model.go
+++ model/model.go
@@ -1,1 +1,58 @@
 package model
+
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
+
+type (
+	userModel struct {
+		gorm.Model
+		Email    string `json:"email"`
+		Password string `json:"password"`
+		Name     string `json:"name"`
+		Goal     string `json:"primary_goal"`
+	}
+
+	transformedUser struct {
+		Email string `json:"email"`
+		Name  string `json:"name"`
+		Goal  string `json:"goal"`
+	}
+
+	workout struct {
+		gorm.Model
+		User  uint      `json:"uid"`
+		Start time.Time `json:"start_time"`
+		End   time.Time `json:"finish_time"`
+	}
+
+	exercise struct {
+		gorm.Model
+		Name string `json:"ex_name"`
+		Link string `json:"info_url"`
+	}
+
+	workoutExercise struct {
+		gorm.Model
+		WorkoutID  uint `json:"workout_id"`
+		ExerciseID uint `json:"exercise_id"`
+	}
+
+	workoutExerciseSet struct {
+		gorm.Model
+		WorkoutExerciseID uint `json:"workout_exercise_id"`
+		Weight            uint `json:"weight"`
+		RepsAttempted     uint `json:"reps_att"`
+		RepsCompleted     uint `json:"reps_comp"`
+	}
+
+	secondaryGoal struct {
+		gorm.Model
+		SetDate    time.Time `json:"set_date"`
+		GoalDate   time.Time `json:"goal_date"`
+		Exercise   uint      `json:"exercise_id"`
+		GoalWeight uint      `json:"goal_weight"`
+	}
+)
